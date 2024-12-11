830470c813dfc2c28e6357a61432c302bd2e4dcc
Normich ITOUA <nitoua.dev@gmail.com>
2024-12-12 00:08:32 +0100

    Update task 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+// routes/index.js
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 // Define routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
