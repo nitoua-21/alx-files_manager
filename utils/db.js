5b78841044c6f6fa96f4792a770f63f902fb11aa
Normich ITOUA <nitoua.dev@gmail.com>
2024-12-12 00:01:17 +0100

    Update task 1

--- utils/db.js
+++ utils/db.js
@@ -3,14 +3,18 @@ import { MongoClient } from 'mongodb';
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
+    const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
 
     this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.database = database;
+
     this.client.connect((err) => {
       if (err) {
-        console.error('MongoDB Connection Error:', err);
+        console.error(`MongoDB connection error: ${err}`);
+      } else {
+        console.log('Connected to MongoDB');
       }
     });
   }
@@ -20,12 +24,12 @@ class DBClient {
   }
 
   async nbUsers() {
-    const db = this.client.db();
+    const db = this.client.db(this.database);
     return db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    const db = this.client.db();
+    const db = this.client.db(this.database);
     return db.collection('files').countDocuments();
   }
 }
