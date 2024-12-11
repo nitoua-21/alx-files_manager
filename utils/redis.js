592d1912af2c883b1b2797f1b7b4f26b79051a75
Normich ITOUA <nitoua.dev@gmail.com>
2024-12-11 23:37:16 +0100

    Add Task 0

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error('Redis Client Error:', error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    return setAsync(key, duration, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
